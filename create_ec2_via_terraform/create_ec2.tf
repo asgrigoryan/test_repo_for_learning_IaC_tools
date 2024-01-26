2b7010ce1ca85d3edaadd0c49a06d16507d725c3
astghikgrigoryanpcln <130073159+astghikgrigoryanpcln@users.noreply.github.com>
2024-01-26 16:17:21 +0400

    Adding files to create ec2 with terraform

---
+++ create_ec2_via_terraform/create_ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = "vpc-0525574ad257c4b46"
+  cidr_block        = "173.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-00ee4df451840fa9d"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-07ce1861791c150af"
+  associate_public_ip_address = "true"
+  tags = {
+    Name = "ec2-example-terraform"
+  }
+}
