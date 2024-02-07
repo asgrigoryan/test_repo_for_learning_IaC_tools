ccc7034e124b5d57385258551d7e6bc628d6dacb
asgrigoryan <148821211+asgrigoryan@users.noreply.github.com>
2024-02-07 21:33:39 +0400

    Update create_ec2.tf

--- create_ec2.tf
+++ create_ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-00ee4df451840fa9d"
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
+  vpc_id = aws_vpc.default.id
   tags = {
     Name = "ec2-example-terraform"
   }
