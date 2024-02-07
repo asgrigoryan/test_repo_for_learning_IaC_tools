393c6cca09b0f447417fcfd316304e5a68c2b6c9
astghikgrigoryanpcln <130073159+astghikgrigoryanpcln@users.noreply.github.com>
2024-02-07 20:48:13 +0400

    Refactoring

--- create_ec2.tf
+++ create_ec2.tf
@@ -1,23 +1,3 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = "vpc-0525574ad257c4b46"
-  cidr_block        = "173.16.10.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "tf-example"
-  }
-  }
-}
-
-
 resource "aws_instance" "web" {
   ami           = "ami-00ee4df451840fa9d"
   instance_type = "t2.micro"
