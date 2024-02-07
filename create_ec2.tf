5ddab6329f222a6d08d8272d2e52b4438ef0da55
asgrigoryan <148821211+asgrigoryan@users.noreply.github.com>
2024-02-07 22:08:06 +0400

    Update create_ec2.tf

--- create_ec2.tf
+++ create_ec2.tf
@@ -23,6 +23,6 @@ resource "aws_instance" "web" {
   subnet_id = "subnet-07ce1861791c150af"
   associate_public_ip_address = "true"
   tags = {
-    Name = "ec2-example-terraform"
+    Name = "ec2-example-terraform-2"
   }
 }
